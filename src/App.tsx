079624a088f97fdcdf91ffa67a7717044ef4b4ef
jp0624 <jp0624@gmail.com>
2024-04-16 08:13:36 -0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<h1>eVivids</h1>
+			<p className='read-the-docs'>Coming Soon</p>
+		</>
+	)
+}
+
+export default App
