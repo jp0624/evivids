15d5ca19935e87c3b5b8dbd91fe4e86eede9061e
jp0624 <jp0624@gmail.com>
2024-07-23 19:19:26 -0400

    added functionality to form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import eLogo from '/logos/evivids-white.svg'
 import './App.scss'
+
+import { useState, useEffect, useRef } from 'react'
 import emailjs from '@emailjs/browser'
-import { useRef } from 'react'
 
 function App() {
-	const form = useRef()
+	useEffect(() => emailjs.init('YOUR-PUBLIC-KEY-HERE'), [])
+	const emailRef = useRef<HTMLInputElement>()
+	const firstNameRef = useRef<HTMLInputElement>()
+	const lastNameRef = useRef<HTMLInputElement>()
+	const [loading, setLoading] = useState(false)
 
-	const sendEmail = (e) => {
+	const [messageSent, setMessageSent] = useState(false)
+
+	const handleSubmit = async (e: { preventDefault: () => void }) => {
 		e.preventDefault()
-		// service_id, templte_id and public key will get from Emailjs website when you create account and add template service and email service
-		emailjs
-			.sendForm(
-				'YOUR_SERVICE_ID',
-				'YOUR_TEMPLATE_ID',
-				form.current,
-				'YOUR_PUBLIC_KEY'
-			)
-			.then(
-				(result) => {
-					console.log(result.text)
+		console.log('attempting to send email')
+		const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
+		const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+		try {
+			setLoading(true)
+			await emailjs.send(
+				serviceId,
+				templateId,
+				{
+					first_name: firstNameRef.current!.value,
+					last_name: lastNameRef.current!.value,
+					recipient: emailRef.current!.value,
 				},
-				(error) => {
-					console.log(error.text)
+				{
+					publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
 				}
 			)
+			setMessageSent(true)
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoading(false)
+		}
 	}
 
 	return (
@@ -35,19 +49,53 @@ function App() {
 							<img src={eLogo} className='logo' alt='Vite logo' />
 						</li>
 						<li>
-							<form className='form-getintouch' ref={form} onSubmit={sendEmail}>
-								<h2>Get in touch</h2>
-								<pre>
-									import.meta.env.VITE_API_KEY:
-									{import.meta.env.VITE_API_KEY}
-								</pre>
-								<label htmlFor='first-name'>First Name</label>
-								<input type='text' name='first-name' id='first-name' required />
-								<label htmlFor='last-name'>Last Name</label>
-								<input type='text' name='last-name' id='last-name' required />
-								<label htmlFor='email'>Email</label>
-								<input type='email' name='email' id='email' required />
-								<button type='submit'>Submit</button>
+							<form className='form-getintouch' onSubmit={handleSubmit}>
+								{messageSent ? (
+									<>
+										<h2>Thank you!</h2>
+										<p>We will be in touch shortly</p>
+									</>
+								) : (
+									<>
+										<h2>Get in touch</h2>
+										<label htmlFor='first-name'>First Name</label>
+										<input
+											ref={(ref) =>
+												(firstNameRef.current = ref as HTMLInputElement)
+											}
+											type='text'
+											name='first-name'
+											id='first-name'
+											required
+										/>
+										<label htmlFor='last-name'>Last Name</label>
+										<input
+											ref={(ref) =>
+												(lastNameRef.current = ref as HTMLInputElement)
+											}
+											type='text'
+											name='last-name'
+											id='last-name'
+										/>
+										<label htmlFor='email'>Email</label>
+										<input
+											ref={(ref) =>
+												(emailRef.current = ref as HTMLInputElement)
+											}
+											type='email'
+											name='email'
+											id='email'
+											required
+										/>
+										<button
+											type='submit'
+											className={loading ? 'loading' : ''}
+											disabled={loading}
+										>
+											Submit
+										</button>
+									</>
+								)}
 							</form>
 						</li>
 					</ul>
