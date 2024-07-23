3e53753608f7ea2ec7a3b1c6980f89591a81de10
jp0624 <jp0624@gmail.com>
2024-07-23 18:00:26 -0400

    Stopped tracking env.local, and env.staging

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import eLogo from '/logos/evivids-white.svg'
 import './App.scss'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 function App() {
+	const form = useRef()
+
+	const sendEmail = (e) => {
+		e.preventDefault()
+		// service_id, templte_id and public key will get from Emailjs website when you create account and add template service and email service
+		emailjs
+			.sendForm(
+				'YOUR_SERVICE_ID',
+				'YOUR_TEMPLATE_ID',
+				form.current,
+				'YOUR_PUBLIC_KEY'
+			)
+			.then(
+				(result) => {
+					console.log(result.text)
+				},
+				(error) => {
+					console.log(error.text)
+				}
+			)
+	}
+
 	return (
 		<>
 			<div className='site-container'>
@@ -11,8 +35,12 @@ function App() {
 							<img src={eLogo} className='logo' alt='Vite logo' />
 						</li>
 						<li>
-							<form className='form-getintouch'>
+							<form className='form-getintouch' ref={form} onSubmit={sendEmail}>
 								<h2>Get in touch</h2>
+								<pre>
+									import.meta.env.VITE_API_KEY:
+									{import.meta.env.VITE_API_KEY}
+								</pre>
 								<label htmlFor='first-name'>First Name</label>
 								<input type='text' name='first-name' id='first-name' required />
 								<label htmlFor='last-name'>Last Name</label>
