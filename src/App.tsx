7decc2ec107a8f94b9a2c3aec56c0bc67d647692
jp0624 <jp0624@gmail.com>
2024-07-23 17:12:13 -0400

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,23 @@ function App() {
 		<>
 			<div className='site-container'>
 				<section className='section-main'>
-					<img src={eLogo} className='logo' alt='Vite logo' />
+					<ul>
+						<li>
+							<img src={eLogo} className='logo' alt='Vite logo' />
+						</li>
+						<li>
+							<form className='form-getintouch'>
+								<h2>Get in touch</h2>
+								<label htmlFor='first-name'>First Name</label>
+								<input type='text' name='first-name' id='first-name' required />
+								<label htmlFor='last-name'>Last Name</label>
+								<input type='text' name='last-name' id='last-name' required />
+								<label htmlFor='email'>Email</label>
+								<input type='email' name='email' id='email' required />
+								<button type='submit'>Submit</button>
+							</form>
+						</li>
+					</ul>
 				</section>
 			</div>
 		</>
