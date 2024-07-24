959e7cf27f1d9b07064ad04f8f0c3218d1255fc8
jp0624 <jp0624@gmail.com>
2024-07-24 11:47:10 -0400

    tweak

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,9 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig(({ command, mode }) => {
-	// Load env file based on `mode` in the current working directory.
-	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
 	const env = loadEnv(mode, process.cwd(), '')
 	return {
 		plugins: [react()],
-		// vite config
 		define: {
 			__APP_ENV__: JSON.stringify(env.APP_ENV),
 		},
