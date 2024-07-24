c2f211348fb630f7dfb7e6bc81405e693c12aeb5
jp0624 <jp0624@gmail.com>
2024-07-23 21:49:08 -0400

    added pages to form

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,20 @@ function App() {
 	const emailRef = useRef<HTMLInputElement>()
 	const firstNameRef = useRef<HTMLInputElement>()
 	const lastNameRef = useRef<HTMLInputElement>()
+	const messageRef = useRef<HTMLTextAreaElement>()
+
 	const [loading, setLoading] = useState(false)
 
 	const [messageSent, setMessageSent] = useState(false)
+	const [formPage, setFormPageChage] = useState(1)
+	const formPages = 2
 
 	const handleSubmit = async (e: { preventDefault: () => void }) => {
 		e.preventDefault()
+		console.log('formPage: ', formPage)
+		if (formPage < formPages) {
+			return setFormPageChage(formPage + 1)
+		}
 		console.log('attempting to send email')
 		const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
 		const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
@@ -58,41 +66,63 @@ function App() {
 								) : (
 									<>
 										<h2>Get in touch</h2>
-										<label htmlFor='first-name'>First Name</label>
-										<input
-											ref={(ref) =>
-												(firstNameRef.current = ref as HTMLInputElement)
-											}
-											type='text'
-											name='first-name'
-											id='first-name'
-											required
-										/>
-										<label htmlFor='last-name'>Last Name</label>
-										<input
-											ref={(ref) =>
-												(lastNameRef.current = ref as HTMLInputElement)
-											}
-											type='text'
-											name='last-name'
-											id='last-name'
-										/>
-										<label htmlFor='email'>Email</label>
-										<input
-											ref={(ref) =>
-												(emailRef.current = ref as HTMLInputElement)
-											}
-											type='email'
-											name='email'
-											id='email'
-											required
-										/>
+										<div className={`form-pages showPage-${formPage}`}>
+											<div
+												className={`page page-${
+													formPage === 1 ? 'show' : 'hide'
+												}`}
+											>
+												<label htmlFor='first-name'>First Name</label>
+												<input
+													ref={(ref) =>
+														(firstNameRef.current = ref as HTMLInputElement)
+													}
+													type='text'
+													name='first-name'
+													id='first-name'
+													required
+												/>
+												<label htmlFor='last-name'>Last Name</label>
+												<input
+													ref={(ref) =>
+														(lastNameRef.current = ref as HTMLInputElement)
+													}
+													type='text'
+													name='last-name'
+													id='last-name'
+												/>
+												<label htmlFor='email'>Email</label>
+												<input
+													ref={(ref) =>
+														(emailRef.current = ref as HTMLInputElement)
+													}
+													type='email'
+													name='email'
+													id='email'
+													required
+												/>
+											</div>
+											<div
+												className={`page page-${
+													formPage === 2 ? 'show' : 'hide'
+												}`}
+											>
+												<label htmlFor='your-message'>Your Message</label>
+												<textarea
+													ref={(ref) =>
+														(messageRef.current = ref as HTMLTextAreaElement)
+													}
+													name='your-message'
+													id='your-message'
+												></textarea>
+											</div>
+										</div>
 										<button
 											type='submit'
 											className={loading ? 'loading' : ''}
 											disabled={loading}
 										>
-											Submit
+											{formPage < formPages ? 'Next >' : 'Submit'}
 										</button>
 									</>
 								)}
